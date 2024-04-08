14383039cdb111e242d675cecb88190e614d60ff
MitchWAldrich <mitch.w.aldrich@gmail.com>
2024-04-08 15:13:33 -0400

    clean up code and update click logic for final numnber choice

--- src/components/Board3.tsx
+++ src/components/Board3.tsx
@@ -13,87 +13,19 @@ import {BoardProps, cellObject, puzzleArray} from '../utils/types';
 
 import Cell3 from './Cell3';
 
-import {removeStringItemFromArray, sortNumbers} from '../utils/helpers';
+import {
+  formatColumn,
+  formatRow,
+  sortNumbers,
+  updatePuzzle,
+} from '../utils/helpers';
 
 const Board3: React.FC<BoardProps> = ({puzzle}) => {
   const [clickType, setClickType] = useState<string>('default');
   const [number, setNumber] = useState<string | number | null>(null);
   const [currentCell, setCurrentCell] = useState<string | number | null>(null);
-  const [numberValue, setNumberValue] = useState<string | number | null>(null);
-  const [isDraft, setIsDraft] = useState<boolean>(false);
-  const [isFinal, setIsFinal] = useState<boolean>(false);
-
-  const rows = [
-    puzzle.filter(cell => cell.row === 2 && cell.target === true),
-    puzzle.filter(cell => cell.row === 3 && cell.target === true),
-    puzzle.filter(cell => cell.row === 4 && cell.target === true),
-    puzzle.filter(cell => cell.row === 5 && cell.target === true),
-    puzzle.filter(cell => cell.row === 6 && cell.target === true),
-    puzzle.filter(cell => cell.row === 7 && cell.target === true),
-    puzzle.filter(cell => cell.row === 8 && cell.target === true),
-    puzzle.filter(cell => cell.row === 9 && cell.target === true),
-    puzzle.filter(cell => cell.row === 10 && cell.target === true),
-    puzzle.filter(cell => cell.row === 11 && cell.target === true),
-  ];
-
-  const formatColumn = (column: cellObject[]) => {
-    const newArray: cellObject[][] = [];
-
-    for (let i = 0; i < column.length; i++) {
-      if (column[i].target === true) {
-        const array: cellObject[] = [column[i]];
-
-        while (
-          column[i + 1] &&
-          column[i + 1].id === (Number(column[i]?.id) + 11).toString() &&
-          column[i + 1].target === true
-        ) {
-          array.push(column[i + 1]);
-          i++;
-        }
-        newArray.push(array);
-      }
-    }
-
-    return newArray;
-  };
-
-  const formatRow = (row: cellObject[]) => {
-    const newArray: cellObject[][] = [];
-
-    for (let i = 0; i < row.length; i++) {
-      if (row[i].target === true) {
-        const array: cellObject[] = [row[i]];
-
-        while (
-          row[i + 1] &&
-          row[i + 1].id === (Number(row[i]?.id) + 1).toString() &&
-          row[i + 1].target === true
-        ) {
-          array.push(row[i + 1]);
-          i++;
-        }
-        newArray.push(array);
-      }
-    }
-
-    return newArray;
-  };
-
-  const columns = [
-    puzzle.filter(cell => cell.column === 2 && cell.target === true),
-    puzzle.filter(cell => cell.column === 3 && cell.target === true),
-    puzzle.filter(cell => cell.column === 4 && cell.target === true),
-    puzzle.filter(cell => cell.column === 5 && cell.target === true),
-    puzzle.filter(cell => cell.column === 6 && cell.target === true),
-    puzzle.filter(cell => cell.column === 7 && cell.target === true),
-    puzzle.filter(cell => cell.column === 8 && cell.target === true),
-    puzzle.filter(cell => cell.column === 9 && cell.target === true),
-    puzzle.filter(cell => cell.column === 10 && cell.target === true),
-    puzzle.filter(cell => cell.column === 11 && cell.target === true),
-  ];
-
   const [currentPuzzle, setCurrentPuzzle] = useState<puzzleArray>(puzzle);
+
   const [currentRows, setCurrentRows] = useState<cellObject[][][]>([
     formatRow(puzzle.filter(cell => cell.row === 2 && cell.target === true)),
     formatRow(puzzle.filter(cell => cell.row === 3 && cell.target === true)),
@@ -147,6 +79,7 @@ const Board3: React.FC<BoardProps> = ({puzzle}) => {
     numberVal: string | number | null,
   ) => {
     setCurrentCell(id);
+    setCurrentPuzzle(updatePuzzle(currentPuzzle, id, numberVal));
     console.log('id and number', id, numberVal);
   };
 
