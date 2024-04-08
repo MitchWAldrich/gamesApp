21b789cd7905bc90b547b8a227bc8110ffd9dc9e
MitchWAldrich <mitch.w.aldrich@gmail.com>
2024-04-08 17:33:58 -0400

    format cell for multiple number display

--- src/components/Cell3.tsx
+++ src/components/Cell3.tsx
@@ -1,35 +1,70 @@
-import {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import Animated, {useAnimatedStyle} from 'react-native-reanimated';
 
 import {CellProps} from '../utils/types';
 
-const Cell3: React.FC<CellProps> = ({
-  cellObj,
-  currentCell,
-  finalNumber,
-  hitArray,
-}) => {
-  const [cellValue, setCellValue] = useState<
-    string | number | null | undefined
-  >(null);
-
+const Cell3: React.FC<CellProps> = ({cellObj}) => {
   const animatedText = useAnimatedStyle(() => {
     return {
-      color: hitArray?.includes(cellObj.id)
-        ? 'green'
-        : cellObj.isLabel === true
-        ? ''
-        : 'black',
+      color:
+        cellObj.clickType === 'final'
+          ? 'green'
+          : cellObj.clickType === 'draft'
+          ? 'blue'
+          : 'black',
+      fontSize: cellObj.clickType === 'draft' ? 9 : 15,
     };
   });
 
+  const animatedCell = useAnimatedStyle(() => {
+    return {
+      justifyContent: cellObj.clickType === 'draft' ? 'flex-start' : 'center',
+      alignItems: cellObj.clickType === 'draft' ? 'center' : 'center',
+      padding: cellObj.clickType === 'draft' ? 3 : 0,
+    };
+  });
+
+  const formatDraftDisplay = (
+    labelValue: string[] | number[] | null | undefined,
+  ) => {
+    if (!labelValue) return;
+
+    //update so each number has a static position
+    let row1 = [labelValue[0], labelValue[1], labelValue[2]];
+    let row2 =
+      labelValue.length > 3
+        ? [labelValue[3], labelValue[4], labelValue[5]]
+        : null;
+    let row3 =
+      labelValue.length > 5
+        ? [labelValue[6], labelValue[7], labelValue[8]]
+        : null;
+
+    const displayValue = (
+      <>
+        <Animated.Text style={[styles.text, animatedText]}>
+          {row1}
+        </Animated.Text>
+        {labelValue.length > 1 && (
+          <Animated.Text style={[styles.text, animatedText]}>
+            {row2}
+          </Animated.Text>
+        )}
+        {labelValue.length > 3 && (
+          <Animated.Text style={[styles.text, animatedText]}>
+            {row3}
+          </Animated.Text>
+        )}
+      </>
+    );
+
+    return displayValue;
+  };
+
   return (
-    <View style={styles.cell}>
-      <Animated.Text style={[styles.text, animatedText]}>
-        {cellObj.label}
-      </Animated.Text>
-    </View>
+    <Animated.View style={[styles.cell, animatedCell]}>
+      {formatDraftDisplay(cellObj.label)}
+    </Animated.View>
   );
 };
 
@@ -43,8 +78,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   cell: {
-    height: 30,
-    width: 30,
+    display: 'flex',
+    flexDirection: 'column',
+    height: 38,
+    width: 38,
     borderStyle: 'solid',
     borderWidth: 1,
     justifyContent: 'center',
@@ -53,13 +90,13 @@ const styles = StyleSheet.create({
   },
   miss: {
     backgroundColor: 'green',
-    height: 20,
-    width: 20,
+    height: 34,
+    width: 34,
     margin: 5,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {},
+  text: {fontSize: 15},
 });
 
 export default Cell3;
