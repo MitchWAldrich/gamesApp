a32f20360491e6083947b3baa49d7c9c12885704
MitchWAldrich <mitch.w.aldrich@gmail.com>
2024-04-22 16:38:51 -0400

    move styles

--- src/components/Cell3.tsx
+++ src/components/Cell3.tsx
@@ -1,7 +1,7 @@
-import {StyleSheet} from 'react-native';
 import Animated, {useAnimatedStyle} from 'react-native-reanimated';
 
 import {CellProps} from '../utils/types';
+import {styles} from '../styles/cell3styles';
 
 const Cell3: React.FC<CellProps> = ({cellObj}) => {
   const animatedText = useAnimatedStyle(() => {
@@ -96,51 +96,5 @@ const Cell3: React.FC<CellProps> = ({cellObj}) => {
     </Animated.View>
   );
 };
-
-const styles = StyleSheet.create({
-  top_container: {
-    backgroundColor: 'red',
-  },
-  flexrow_container: {
-    flexDirection: 'row',
-    backgroundColor: 'blue',
-    justifyContent: 'center',
-  },
-  cell: {
-    display: 'flex',
-    flexDirection: 'column',
-    height: 38,
-    width: 38,
-    borderStyle: 'solid',
-    borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
-  inner_cell: {
-    display: 'flex',
-    flexDirection: 'row',
-    height: 10,
-    width: 10,
-    justifyContent: 'center',
-  },
-  inner_cell_container: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  inner_cell_row: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  miss: {
-    backgroundColor: 'green',
-    height: 34,
-    width: 34,
-    margin: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {fontSize: 15},
-});
 
 export default Cell3;
